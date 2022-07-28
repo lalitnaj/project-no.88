52c76333a72eaa8a378cee4c9921d011ea921924
lalitnaj <86791474+lalitnaj@users.noreply.github.com>
2022-07-28 18:36:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCl2WvMP_UAjkirvf8fXIuKjs302mqzDHs",
+  authDomain: "story-telllingg-app.firebaseapp.com",
+  databaseURL: "https://story-telllingg-app-default-rtdb.firebaseio.com",
+  projectId: "story-telllingg-app",
+  storageBucket: "story-telllingg-app.appspot.com",
+  messagingSenderId: "661502667766",
+  appId: "1:661502667766:web:b680e629084971a2c5f5d7"
+};
